a63bcc10f61207ef361a89d33d9551b4b097f28e
RishivinKannan <rishivinkannanrrk003@gmail.com>
2024-02-03 10:20:56 +0530

    Recent History added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import { NavLink } from "react-router-dom";
-import { useState, Fragment, useContext} from "react";
+import { useState, Fragment, useContext } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { UserDetailsContext } from "../App";
+import SearchBox from "./SearchBox";
 import {
   SearchIcon,
   WishListIcon,
@@ -16,10 +17,14 @@ const Header = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [searchBox, setSearchBox] = useState(false);
   const [LoginBox, setLoginBox] = useState(false);
-  const userDetails=useContext(UserDetailsContext);
+  const userDetails = useContext(UserDetailsContext);
 
-  function userAuthentication(userNameValue,passwordValue) {
-    userDetails.provider({...userDetails,username:userNameValue,password:passwordValue});
+  function userAuthentication(userNameValue, passwordValue) {
+    userDetails.provider({
+      ...userDetails,
+      username: userNameValue,
+      password: passwordValue,
+    });
     setLoggedIn(true);
     setLoginBox(false);
   }
@@ -33,11 +38,13 @@ const Header = () => {
       </NavLink>
 
       <div
-        className={`bg-black absolute top-20 w-full left-0 p-4 lg:max-2xl:static lg:max-2xl:w-96 lg:max-2xl:p-0 lg:max-2xl:block ${
+        className={`bg-black absolute top-20 w-full left-0 p-4 lg:max-2xl:relative lg:max-2xl:top-0 lg:max-2xl:w-96 lg:max-2xl:p-0 lg:max-2xl:block ${
           searchBox ? "" : "hidden"
         }`}
       >
-        <div className="p-2 flex bg-white text-black lg:max-2xl:w-96 rounded-lg">
+        <SearchBox />
+
+        {/* <div className="p-2 flex bg-white text-black lg:max-2xl:w-full rounded-lg">
           <SearchIcon />
           <input
             type="text"
@@ -48,10 +55,9 @@ const Header = () => {
                 alert("You pressed Enter");
               }
             }}
-          ></input>
-        </div>
+          />
+        </div> */}
       </div>
-
 
       <div className="flex">
         <div className="p-1 flex space-x-4 md:max-2xl:p-2 md:max-2xl:space-x-6 items-center">
@@ -75,9 +81,7 @@ const Header = () => {
           </NavLink>
         </div>
 
-
         {/* User */}
-
 
         {loggedIn ? (
           <Popover className="relative">
@@ -85,14 +89,18 @@ const Header = () => {
               <>
                 <Popover.Button
                   className={`ml-6 flex items-center justify-between rounded md:max-2xl:p-3 hover:outline-gray-200 ${
-                    open ? "md:max-2xl:outline" : "md:max-2xl:hover:outline md:max-2xl:hover:outline-1"
+                    open
+                      ? "md:max-2xl:outline"
+                      : "md:max-2xl:hover:outline md:max-2xl:hover:outline-1"
                   }`}
                 >
-                  {open?setSearchBox(false):""}
+                  {open ? setSearchBox(false) : ""}
                   {open ? <UserSolidIcon className="w-8 h-8" /> : <UserIcon />}
 
                   <span className="ml-2 font-semibold text-lg hidden md:max-2xl:inline">
-                  {userDetails.username==""?"Username":userDetails.username}
+                    {userDetails.username == ""
+                      ? "Username"
+                      : userDetails.username}
                   </span>
                 </Popover.Button>
 
@@ -105,30 +113,44 @@ const Header = () => {
                   leaveFrom="opacity-100 translate-y-0"
                   leaveTo="opacity-0 translate-y-1"
                 >
-                  <Popover.Panel className={`absolute z-[100] text-black w-80  right-0 mt-1 top-20`}>
+                  <Popover.Panel
+                    className={`absolute z-[100] text-black w-80  right-0 mt-1 top-20`}
+                  >
                     <div className="overflow-hidden rounded-lg shadow-xl ring-1 ring-black/5">
                       <div className="flex justify-start items-center space-x-4 p-6">
-                        <UserSolidIcon className="w-11 h-11"/>
+                        <UserSolidIcon className="w-11 h-11" />
                         <div>
-                        <span className="ml-2 font-semibold text-lg  ">
-                          {userDetails.username==""?"username":userDetails.username}
-                        </span>
-                        <p className="ml-2">usermail@email.com</p>
+                          <span className="ml-2 font-semibold text-lg  ">
+                            {userDetails.username == ""
+                              ? "username"
+                              : userDetails.username}
+                          </span>
+                          <p className="ml-2">usermail@email.com</p>
                         </div>
                       </div>
-                      <hr className="bg-gray-200 h-[2px] w-full"/>
+                      <hr className="bg-gray-200 h-[2px] w-full" />
                       <div className="py-2">
                         <ul>
-                          <li className="px-4 py-2 font-semibold hover:bg-gray-300 hover:text-gray-600">Account</li>
-                          <li className="px-4 py-2 font-semibold hover:bg-gray-300 hover:text-gray-600">Orders</li>
-                          <li className="px-4 py-2 font-semibold hover:bg-gray-300 hover:text-gray-600">History</li>
-                          <li className="px-4 py-2 font-semibold hover:bg-gray-300 hover:text-gray-600">Saved Address</li>
-                          <li className="px-4 py-2 font-semibold hover:bg-gray-300 hover:text-gray-600"
-                          onClick={()=>setLoggedIn(false)}>Logout</li>
+                          <li className="px-4 py-2 font-semibold hover:bg-gray-300 hover:text-gray-600">
+                            Account
+                          </li>
+                          <li className="px-4 py-2 font-semibold hover:bg-gray-300 hover:text-gray-600">
+                            Orders
+                          </li>
+                          <li className="px-4 py-2 font-semibold hover:bg-gray-300 hover:text-gray-600">
+                            History
+                          </li>
+                          <li className="px-4 py-2 font-semibold hover:bg-gray-300 hover:text-gray-600">
+                            Saved Address
+                          </li>
+                          <li
+                            className="px-4 py-2 font-semibold hover:bg-gray-300 hover:text-gray-600"
+                            onClick={() => setLoggedIn(false)}
+                          >
+                            Logout
+                          </li>
                         </ul>
                       </div>
-
-
                     </div>
                   </Popover.Panel>
                 </Transition>
@@ -138,7 +160,10 @@ const Header = () => {
         ) : (
           <button
             className="ml-6 px-4 font-bold text-lg bg-white text-black rounded hover:bg-gray-200 hover:text-gray-500"
-            onClick={() => setLoginBox(true)}
+            onClick={() => {
+              setLoginBox(true);
+              setSearchBox(false);
+            }}
           >
             Login
           </button>
@@ -146,8 +171,6 @@ const Header = () => {
       </div>
 
       <Login open={LoginBox} close={setLoginBox} submit={userAuthentication} />
-
-
     </div>
   );
 };
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,103 @@
+import { SearchIcon, History } from "../utils/Icons";
+import { useEffect, useState } from "react";
+export default function SearchBox() {
+  const [inputValue, setInputValue] = useState("");
+  const [historyValue, setHistoryValue] = useState([]);
+  const [inputFocus, setInputFocus] = useState(false);
+
+  useEffect(() => {
+    const local = localStorage.getItem("recentHistory")
+      ? JSON.parse(localStorage.getItem("recentHistory"))
+      : [];
+    setHistoryValue(local);
+    console.log(local);
+  }, []);
+
+  
+
+  return (
+    <>
+      <div className="p-2 flex bg-white text-black w-full rounded-lg" 
+      >
+        <SearchIcon />
+        <input
+          type="text"
+          placeholder="Search..."
+          className="outline-none ml-2 font-semibold tracking-wide "
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              const historyAdder = historyValue.concat(inputValue);
+              localStorage.setItem(
+                "recentHistory",
+                JSON.stringify(historyAdder)
+              );
+              setHistoryValue(historyAdder);
+            }
+            if (e.key === "Escape") {
+                setInputFocus(false);
+              }
+          }}
+          onFocus={()=>setInputFocus(true)}
+        />
+        {historyValue.length === 0 || (!inputFocus) ? (
+        ""
+      ) : (
+        <div className="absolute w-[95%] translate-y-8 left-3  bg-white text-black mt-2 rounded-xl shadow-2xl lg:max-2xl:translate-y-7 lg:max-2xl:left-0 lg:max-2xl:w-96 z-[100] pb-3">
+          <div className="text-sm font-semibold text-purple-800 tracking-wide flex flex-col">
+            <div className="px-3 pt-2"> <span className="inline tracking-wider text-black float-left" >Recent Search</span>
+            <span className=" text-red-700  cursor-pointer float-right" onClick={()=>setInputFocus(false)}>X</span></div>
+           
+            {inputValue === ""
+              ? historyValue.toReversed().map((history, index, arr) => {
+                  return index < 5 ?(
+                    <div
+                      key={index + history}
+                      className="text-xs font-semibold  tracking-wide hover:bg-gray-300 "
+                      onClick={()=>{setInputValue(history);
+                    }}
+                    >
+                      <button className="flex items-center justify-start space-x-2 py-3 px-5"
+                        >
+                        <History className={"w-4 h-3 text-black"} />
+                        <span >{history}</span>
+                      </button>
+                      {arr.length - 1 === index ? (
+                        ""
+                      ) : (
+                        <hr className="bg-gray-200 h-[2px] w-full" />
+                      )} 
+                    </div>
+                  ): null
+                })
+              : historyValue.toReversed().map((history, index, arr) => {
+                 
+                  return history.toLowerCase().includes(inputValue.toLowerCase()) ? (
+
+                    <div
+                      key={index + history}
+                      className="text-xs font-semibold  tracking-wide hover:bg-gray-300 "
+                      onClick={()=>{setInputValue(history);
+                        }}
+                    >
+                      <div className="flex items-center justify-start space-x-2 py-3 px-5">
+                        <History className={"w-4 h-3 text-black"} />
+                        <button>{history}</button>
+                      </div>
+                      {arr.length - 1 === index ? (
+                        ""
+                      ) : (
+                        <hr className="bg-gray-200 h-[2px] w-full" />
+                      )}
+                    </div>
+                  ) : null
+                }  )}
+          </div>
+        </div>
+      )}
+      </div>
+      
+    </>
+  );
+}
