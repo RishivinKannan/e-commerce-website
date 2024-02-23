5757b0001bb82960ce5cd6900e2fbc2d8f286bd3
RishivinKannan <rishivinkannanrrk003@gmail.com>
2024-02-23 19:05:43 +0530

    tested userSlice dipatch and useSelector

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,16 +1,17 @@
-import {
-  useGetAllProductsQuery,
-  useGetTopPicksQuery,
-  useGetTopSalesQuery,
-  useGetNewArrivalsQuery,
-} from "./Redux/api/products";
-
+import { useDispatch, useSelector } from "react-redux";
+import {userlogin,userlogout} from './Redux/services/userSlice'
 export default function Test() {
+  const { username, email, isLogged } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
   return (
     <>
-      <div className="h-screen flex justify-center items-center">
-        <h1 className="text-4xl">TEST</h1>
+      <div className="h-screen flex justify-center items-center gap-8">
+        <h1 className="text-4xl">{username}</h1>
+        <h1 className="text-4xl">{email }</h1>
+        <h1 className="text-4xl">{isLogged.toString()}</h1>
+        <button onClick={()=>dispatch(userlogin({username:'rishi',email:'rishi@gmail.com'}))}>login</button>
+        <button onClick={()=>dispatch(userlogout())}>logout</button>
       </div>
     </>
   );
