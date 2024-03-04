00e8564d314673962cc50deb4f3235a781c8c193
RishivinKannan <rishivinkannanrrk003@gmail.com>
2024-03-04 12:36:50 +0530

    welcome audio added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import Header from "./components/Header/Header";
 import { Outlet, ScrollRestoration } from "react-router-dom";
-
-
+import welcome from "./assets/welcome2.mp3";
 function App() {
-
   return (
     <>
-        <ScrollRestoration
-          getKey={(location) => {
-            return location.pathname == "/" ? location.pathname : location.key;
-          }}
-        />
-          <Header />
-          <Outlet />
-      
+      <ScrollRestoration
+        getKey={(location) => {
+          return location.pathname == "/" ? location.pathname : location.key;
+        }}
+      />
+      <audio src={welcome} autoPlay />
+      <Header />
+      <Outlet />
     </>
   );
 }
--- src/CustomerApp.jsx
+++ src/CustomerApp.jsx
@@ -2,9 +2,12 @@ import { Outlet } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./Redux/store";
 
+
 const CustomerApp = () => {
+
   return (
     <Provider store={store}>
+      
       <Outlet />
     </Provider>
   );
