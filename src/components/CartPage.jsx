17d525c5b333abe88f860b118437d111caf3e328
RishivinKannan <rishivinkannanrrk003@gmail.com>
2024-03-04 12:38:34 +0530

    Tailwind classes auto arragements

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -13,14 +13,14 @@ export default function CartPage() {
   }, [username,dispatch]);
   return (
     <>
-      <div className="p-2 pt-28 bg-gray-100 min-h-screen">
+      <div className="min-h-screen p-2 bg-gray-100 pt-28">
         <span className="pl-8 text-3xl font-extrabold tracking-wider leading-9 [text-shadow:1px_1px_3px_var(--tw-shadow-color)] shadow-gray-600">
           Cart
         </span>
-        <div className="p-4 grid grid-cols-3 gap-y-10 md:grid-cols-5">
-          <div className="col-span-3 flex flex-col gap-4 items-center p-4">
+        <div className="grid grid-cols-3 p-4 gap-y-10 md:grid-cols-5">
+          <div className="flex flex-col items-center col-span-3 gap-4 p-4">
             {cartList.length == 0 ? (
-              <div className="w-full h-full flex justify-center items-center text-2xl font-bold tracking-wider text-zinc-600">
+              <div className="flex items-center justify-center w-full h-full text-2xl font-bold tracking-wider text-zinc-600">
                 No Cart Items
               </div>
             ) : (
@@ -32,7 +32,7 @@ export default function CartPage() {
               ))
             )}
           </div>
-          <div className="py-3 flex justify-center col-span-3 md:col-span-2 ">
+          <div className="flex justify-center col-span-3 py-3 md:col-span-2 ">
             <OrderSummary />
           </div>
         </div>
